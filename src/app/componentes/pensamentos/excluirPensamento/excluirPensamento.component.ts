ceba6bd454d857a7d60f5348bd1e90d7f76b578a
Franklin Matos Correia <franklinmatos@gmail.com>
2023-04-18 16:56:22 -0300

    componente de excluir.

---
+++ src/app/componentes/pensamentos/excluirPensamento/excluirPensamento.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pensamento } from 'backend/pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-excluirPensamento',
+  templateUrl: './excluirPensamento.component.html',
+  styleUrls: ['./excluirPensamento.component.css']
+})
+export class ExcluirPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+}
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+  })
+  }
+
+  cancelar(){
+    this.router.navigate(['/listarPensamento'])
+  }
+
+  excluirPensamento(){
+    if(this.pensamento.id) {
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+          this.router.navigate(['/listarPensamento'])
+      })
+  }
+  }
+
+}
