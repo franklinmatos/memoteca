d7c532f43783a294c41dd12e21aed380b88df3d9
Franklin Matos Correia <franklinmatos@gmail.com>
2023-04-25 14:57:01 -0300

    validações nas edições de pensamentos.

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Pensamento } from 'backend/pensamento';
 import { PensamentoService } from '../pensamento.service';
@@ -10,27 +11,34 @@ import { PensamentoService } from '../pensamento.service';
 })
 export class EditarPensamentoComponent implements OnInit {
 
-  pensamento: Pensamento = {
-    id: 0,
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-}
+
+  id: string = '';
+  formulario!: FormGroup;
+
+  pensamento!: Pensamento;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id')
-    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
-        this.pensamento = pensamento
+    this.criarFormulario()
+
+    this.service.buscarPorId(parseInt(this.getId())).subscribe((pensamento) => {
+
+        this.formulario.get('conteudo')?.setValue(pensamento.conteudo)
+        this.formulario.get('autoria')?.setValue(pensamento.autoria)
+        this.formulario.get('id')?.setValue(pensamento.id)
+        this.formulario.get('modelo')?.setValue(pensamento.modelo)
+        console.log('formulario Edit =>> ',this.formulario)
     })
   }
 
   editarPensamento() {
+    this.updatePensamento(this.formulario)
     this.service.editar(this.pensamento).subscribe(() => {
         this.router.navigate(['/listarPensamento'])
     })
@@ -38,6 +46,46 @@ export class EditarPensamentoComponent implements OnInit {
 
   cancelar() {
     this.router.navigate(['/listarPensamento'])
-}
+  }
+
+  criarFormulario(){
+    this.formulario = this.formBuilder.group({
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/), ]
+        )],
+      autoria:['' , Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ]
+        )],
+      modelo: ['modelo3' ]
+    })
+    this.formulario.get('conteudo')?.errors
+  }
+
+  updatePensamento(formulario: FormGroup){
+    this.pensamento = {
+      conteudo: '',
+      autoria: '',
+      modelo: ''
+    }
+    this.pensamento.autoria = formulario.get('autoria')?.value
+    this.pensamento.conteudo = formulario.get('conteudo')?.value
+    this.pensamento.modelo = formulario.get('modelo')?.value
+    this.pensamento.id = Number(this.getId())
+  }
+
+  getId(): string{
+    return this.route.snapshot.paramMap.get('id') || ''
+
+  }
+
+  habilitarBotao(): string{
+    if(this.formulario.valid){
+      return "botao"
+    }else
+    return "botao__desabilitado"
+  }
 
 }
